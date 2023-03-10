98b351e54fbee3c1a771cfa22062fd65603c1c8d
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-10 15:20:33 +0600

    working on all events page.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,12 @@
 import EventList from '@/components/events/event-list';
 import EventSearch from '@/components/events/events-search';
-import { getAllEvents } from '@/styles/dummy-data';
+import { getAllEvents } from '@/helpers/api-util';
+//import { getAllEvents } from '@/styles/dummy-data';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-function AllEventsPage() {
-  const events = getAllEvents();
+function AllEventsPage(props) {
+  const { events } = props;
   const router = useRouter();
 
   function findEventsHandler(year, month) {
@@ -19,6 +20,21 @@ function AllEventsPage() {
       <EventList items={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get events.
+  // You can use any data fetching library
+  const events = await getAllEvents();
+
+  // By returning { props: { events } }, the HomaPage component
+  // will receive `events` as a prop at build time
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
