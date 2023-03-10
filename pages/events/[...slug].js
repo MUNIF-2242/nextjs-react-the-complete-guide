1a55c55938d424109a0de59e84cc911512449b35
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-10 16:51:40 +0600

    working on server side rendering.

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,107 @@
-function FilteredEventsPage() {
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import EventList from '@/components/events/event-list';
+import Button from '@/components/ui/button';
+import ResultsTitle from '@/components/events/results-title';
+import ErrorAlert from '@/components/ui/error-alert';
+import { getFilteredEvents } from '@/styles/dummy-data';
+
+function FilteredEventsPage(props) {
+  const router = useRouter();
+
+  // const filterData = router.query.slug;
+
+  // if (!filterData) {
+  //   return <p className='center'>Loading...</p>;
+  // }
+
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
+
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
+
+  if (props.hasError) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = props.events;
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(props.date.year, props.date.month - 1);
+
   return (
-    <>
-      <h1>Show all filtered events</h1>
-    </>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  const filterData = params.slug;
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return {
+      props: {
+        hasError: true,
+        // notFound:true,
+        // redirect:{
+        //   destination:'/error'
+        // }
+      },
+    };
+  }
+
+  const filteredEvents = await getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    }, // will be passed to the page component as props
+  };
 }
 
 export default FilteredEventsPage;
