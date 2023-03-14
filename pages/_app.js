f3989bf9956a3ce1df81583a3aa205c6851bda7a
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-14 17:10:20 +0600

    initial project setup done.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '@/components/layout/layout';
+import Notification from '@/components/ui/notification';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Notification title='test' message='hell0' status='success' />
     </Layout>
   );
 }
