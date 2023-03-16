3ecdca17bbd3e528fa4baf4d184226e39b4dbed8
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-16 09:42:27 +0600

    initial project setup done.

---
+++ pages/auth.js
@@ -0,0 +1,7 @@
+import AuthForm from '@/compoonents/auth/auth-form';
+
+function AuthPage() {
+  return <AuthForm />;
+}
+
+export default AuthPage;
---
+++ pages/profile.js
@@ -0,0 +1,7 @@
+import UserProfile from '@/compoonents/profile/user-profile';
+
+function ProfilePage() {
+  return <UserProfile />;
+}
+
+export default ProfilePage;
